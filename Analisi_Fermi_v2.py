8ff784f86f33a2136dd42fca81758d6fce60244d
bsanzi <brunosanzi@virgilio.it>
2025-05-30 17:47:03 +0200

    Update Analisi_Fermi_v2.py

--- Analisi_Fermi_v2.py
+++ Analisi_Fermi_v2.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns #per disegnare grafici statistici
 
 # Percorso file Excel
-# percorso_file = r'C:\Users\bsanzi\OneDrive - FIAMM Energy Technology S.p.A\Desktop\Analisi_fermi.xlsx'
-percorso_file = r'C:\Users\Utente\Desktop\Trim_01_25_Linee123.xls.xlsx'
+percorso_file = r'C:\Users\bsanzi\OneDrive - FIAMM Energy Technology S.p.A\Desktop\Trim_01_25_Linee123.xlsx'
+# percorso_file = r'C:\Users\Utente\Desktop\Trim_01_25_Linee123.xls.xlsx'
 linea_da_analizzare = input ('Quale linea analizzo?')
 macchina_rul = input ('Per quale macchina desideri calcolare variabilità della RUL? ')
 
